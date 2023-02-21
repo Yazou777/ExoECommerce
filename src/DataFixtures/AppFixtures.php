768bfebf9af0d90b0a263118d3d7b10ac223aefa
Yazou777 <wilga.kevin@sfr.fr>
2023-02-21 09:59:27 +0100

    fixture categorie/souscategorie

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categorie;
+use App\Entity\SousCategorie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,6 +14,27 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        $categorie1 = new Categorie();
+
+        $categorie1->setNom("Alimentaire");
+
+        $manager->persist($categorie1);
+
+        $sousCategorie1 = new SousCategorie();
+
+        $sousCategorie1->setNom("Frais");
+
+        $manager->persist($sousCategorie1);
+
+        $sousCategorie2 = new SousCategorie();
+
+        $sousCategorie2->setNom("Surgelé");
+
+        $manager->persist($sousCategorie2);
+
+        // Pour associer vos entités
+        $categorie1->addSousCategorie($sousCategorie1);
+        $categorie1->addSousCategorie($sousCategorie2);
         $manager->flush();
     }
 }

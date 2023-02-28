e5e843699711ddfda013d767aa7a0865fec89c4e
Yazou777 <wilga.kevin@sfr.fr>
2023-02-28 11:24:44 +0100

    produit detail

--- src/Controller/GuitaresElectriquesController.php
+++ src/Controller/GuitaresElectriquesController.php
@@ -12,10 +12,23 @@ class GuitaresElectriquesController extends AbstractController
     #[Route('/guitares_electriques/{id}', name: 'app_guitares_electriques')]
     public function index(ProduitRepository $produitRepository, $id): Response
     {
+    $produits = $produitRepository->findBy([
+        "sousCategorie" => $id
+    ]);
+    return $this->render('guitares_electriques/index.html.twig', [
+        'produits' => $produits
+    ]);
+}
+
+
+
+        #[Route('/detail_guitares_electriques/{id}', name: 'app_detail_guitares_electriques')]
+    public function detail(ProduitRepository $produitRepository, $id): Response
+    {
         $produits = $produitRepository->findBy([
-            "sousCategorie" => $id
+            "id" => $id
         ]);
-        return $this->render('guitares_electriques/index.html.twig', [
+        return $this->render('guitares_electriques/detail.html.twig', [
             'produits' => $produits
         ]);
     }

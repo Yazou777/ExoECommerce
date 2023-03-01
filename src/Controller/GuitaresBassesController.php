cedeac53af9e3e755f5b3c00f25fb6e70977ff72
Yazou777 <wilga.kevin@sfr.fr>
2023-03-01 16:11:20 +0100

    if card title

--- src/Controller/GuitaresBassesController.php
+++ src/Controller/GuitaresBassesController.php
@@ -13,12 +13,14 @@ class GuitaresBassesController extends AbstractController
     #[Route('/guitares_basses/{id}', name: 'app_guitares_basses')]
     public function index(SousCategorieRepository $sousCategorieRepository, $id): Response
     {
-
+        
         $sousCategories = $sousCategorieRepository->findBy([
             "categorie" => $id
         ]);
         return $this->render('guitares_basses/index.html.twig', [
-            'sousCategories' => $sousCategories
+            'sousCategories' => $sousCategories,
+           'id' => $id
+           
         ]);
     }
 }

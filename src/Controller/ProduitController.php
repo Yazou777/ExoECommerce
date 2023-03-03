1791ebe22af527cf507b276a369a25f681e22a1c
Yazou777 <wilga.kevin@sfr.fr>
2023-03-03 11:16:04 +0100

    bug produitdetail1 a corriger

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Entity\SousCategorie;
 use App\Repository\ProduitRepository;
 use App\Repository\SousCategorieRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,21 +12,57 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit/{id}', name: 'app_guitares_electriques')]
-    public function index(ProduitRepository $produitRepository, SousCategorieRepository $sousCategorieRepository, $id): Response
+//     #[Route('/produit/{id}', name: 'app_guitares_electriques')]
+//     public function index(ProduitRepository $produitRepository, SousCategorieRepository $sousCategorieRepository, $id): Response
+//     {
+//     $produits = $produitRepository->findBy([
+//         "sousCategorie" => $id
+//     ]);
+
+
+//     $sousCategorie = $sousCategorieRepository->find($id);
+
+//     return $this->render('produit/index.html.twig', [
+//         'produits' => $produits,
+//         "chemin_de_fer" => [
+//             ["name" => $sousCategorie->getCategorie()->getNom(), "link" => "/sous_categorie/".$sousCategorie->getCategorie()->getId()],
+//             ["name" => $sousCategorie->getNom(), "link" => " "],
+//         ]
+//     ]);
+// }
+
+
+
+//         #[Route('/detail_produit/{id}', name: 'app_detail_guitares_electriques')]
+//     public function detail(ProduitRepository $produitRepository, $id): Response
+//     {
+//         $produits = $produitRepository->findBy([
+//             "id" => $id
+//         ]);
+
+//         $produit = $produitRepository->find($id);
+
+//         return $this->render('produit/detail.html.twig', [
+//             'produits' => $produits,
+//             "chemin_de_fer" => [
+//                 ["name" => $produit->getSousCategorie()->getNom(), "link" => "/produit/".$produit->getSousCategorie()->getId()],
+//                 ["name" => $produit->getNom(), "link" => " "],
+//             ]
+//         ]);
+//     }
+
+
+
+    #[Route('/produit/{SousCategorie}', name: 'app_guitares_electriques')]
+    public function index(SousCategorie $SousCategorie,ProduitRepository $produitRepository, SousCategorieRepository $sousCategorieRepository): Response
     {
-    $produits = $produitRepository->findBy([
-        "sousCategorie" => $id
-    ]);
-
-
-    $sousCategorie = $sousCategorieRepository->find($id);
 
     return $this->render('produit/index.html.twig', [
-        'produits' => $produits,
+        "SousCategorie" => $SousCategorie,
         "chemin_de_fer" => [
-            ["name" => $sousCategorie->getCategorie()->getNom(), "link" => "/sous_categorie/".$sousCategorie->getCategorie()->getId()],
-            ["name" => $sousCategorie->getNom(), "link" => " "],
+            ["name" => "Accueil", "link" => "/"],
+            ["name" => $SousCategorie->getCategorie()->getNom(), "link" => "/sous_categorie/".$SousCategorie->getCategorie()->getId()],
+            ["name" => $SousCategorie->getNom(), "link" => " "],
         ]
     ]);
 }
@@ -32,19 +70,17 @@ class ProduitController extends AbstractController
 
 
         #[Route('/detail_produit/{id}', name: 'app_detail_guitares_electriques')]
-    public function detail(ProduitRepository $produitRepository, $id): Response
+    public function detail(Produit $Produit,SousCategorie $SousCategorie, ProduitRepository $produitRepository): Response
     {
-        $produits = $produitRepository->findBy([
-            "id" => $id
-        ]);
-
-        $produit = $produitRepository->find($id);
-
+     
         return $this->render('produit/detail.html.twig', [
-            'produits' => $produits,
+            'Produit' => $Produit,
+            "SousCategorie" => $SousCategorie,
             "chemin_de_fer" => [
-                ["name" => $produit->getSousCategorie()->getNom(), "link" => "/produit/".$produit->getSousCategorie()->getId()],
-                ["name" => $produit->getNom(), "link" => " "],
+                ["name" => "Accueil", "link" => "/"],
+                ["name" => $SousCategorie->getCategorie()->getNom(), "link" => "/sous_categorie/".$SousCategorie->getCategorie()->getId()],
+                ["name" => $Produit->getSousCategorie()->getNom(), "link" => "/produit/".$Produit->getSousCategorie()->getId()],
+                ["name" => $Produit->getNom(), "link" => " "],
             ]
         ]);
     }
--- src/Controller/SousCategorieController.php
+++ src/Controller/SousCategorieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use App\Entity\SousCategorie;
 use App\Repository\CategorieRepository;
 use App\Repository\SousCategorieRepository;
@@ -11,24 +12,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SousCategorieController extends AbstractController
 {
-    #[Route('/sous_categorie/{id}', name: 'app_guitares_basses')]
-    public function index(SousCategorieRepository $sousCategorieRepository,CategorieRepository $categorieRepository, $id): Response
-    {
+    // #[Route('/sous_categorie/{sousCategorie}', name: 'app_guitares_basses')]
+    // public function index(SousCategorie $sousCategorie,CategorieRepository $categorieRepository): Response
+    // {
         
-        $sousCategories = $sousCategorieRepository->findBy([
-            "categorie" => $id
-        ]);
+    //     // $sousCategories = $sousCategorieRepository->findBy([
+    //     //     "categorie" => $id
+    //     // ]);
 
-        $categories = $categorieRepository->find($id);
+    //     // $categories = $categorieRepository->find($id);
      
-        return $this->render('sous_categorie/index.html.twig', [
-            'sousCategories' => $sousCategories,
-           'id' => $id,
-           "chemin_de_fer" => [
-            [ "name" => $categories->getNom(),"link" => "/"]
-           ]
+    //     return $this->render('sous_categorie/index.html.twig', [
+    //         "sousCategorie" => $sousCategorie,
+    //     //    'id' => $id,
+    //     //    "chemin_de_fer" => [
+    //     //     [ "name" => $categories->getNom(),"link" => "/"]
+    //     //    ]
            
-        ]);
+    //      ]);
+    // }
+
+       #[Route('/sous_categorie/{Categorie}', name: 'app_guitares_basses')]
+    public function index(Categorie $Categorie,CategorieRepository $categorieRepository,SousCategorieRepository $sousCategorieRepository): Response
+    {
+
+
+        return $this->render('sous_categorie/index.html.twig', [
+            "Categorie" => $Categorie,
+            "chemin_de_fer" => [
+            [ "name" => "Accueil", "link" => "/"],
+            [ "name" => $Categorie->getNom(),"link" => ""],
+           ]
+         ]);
     }
- 
+
 }
